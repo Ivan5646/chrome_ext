0609c269b57c1d8ea5095ef8947cc1cc9de72be1
Ivan5646 <Mike000027@yandex.ru>
2017-06-13 03:56:46 +0700

    table

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 var processRemoteData = function processRemoteData(result) {
   console.log(result.data);
-  document.getElementById('status').innerHTML = result.data;
+
+  var myArray = result.data;
+  var tr;
+  for (var i = 0; i < myArray.length; i++) {
+      tr = $('<tr/>');
+      tr.append("<td>" + myArray[i].name + "</td>");
+      tr.append("<td>" + myArray[i].domain + "</td>");
+      tr.append("<td>" + myArray[i].message + "</td>");
+      $('table').append(tr);
+  }
+
+  // document.getElementById('status').innerHTML = result.data[0].name;
+  // document.getElementById('status').innerHTML = myArray[0].name;
 };
 
 chrome.storage.local.get('data', processRemoteData);
