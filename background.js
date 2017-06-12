f6fce9a2881a4bbf62e90b8f3aa873f1ef8ad496
Ivan5646 <Mike000027@yandex.ru>
2017-06-13 03:10:07 +0700

    background process

--- background.js
+++ background.js
@@ -1,22 +1,7 @@
-// document.getElementById('status').innerHTML = "Content changed by background.js";
-
-// fetch('http://www.softomate.net/ext/employees/list.json')
-//   .then(result => result.json())
-//   .then(result => {
-//     console.log('The remote data is fetched!');
-//     console.log(result);
-//     dispatch(getRemoteDataEndSuccess(result));        
-//   })
-//   .catch(error => dispatch(getRemoteDataEndFail(error)));
-
 fetch('http://www.softomate.net/ext/employees/list.json').then(function (result) {
   return result.json();
 }).then(function (result) {
-  console.log('The remote data is fetched!');
-  console.log(result);
-  myObj = result[0];
-  document.getElementById('status').innerHTML = "name: " + myObj.name + ", domain: " + myObj.domain;
-  dispatch(getRemoteDataEndSuccess(result));
+  chrome.storage.local.set({ data: result });
 }).catch(function (error) {
-  return dispatch(getRemoteDataEndFail(error));
+  return null;
 });
--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-// document.getElementById('status').innerHTML = "Content changed by popup.js";
+var processRemoteData = function processRemoteData(result) {
+  console.log(result.data);
+  document.getElementById('status').innerHTML = result.data;
+};
+
+chrome.storage.local.get('data', processRemoteData);
+chrome.storage.onChanged.addListener(processRemoteData);
