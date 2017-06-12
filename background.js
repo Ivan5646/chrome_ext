49516e28129ab5aaa8555dfa40020989255f48d4
Ivan5646 <Mike000027@yandex.ru>
2017-06-13 06:08:12 +0700

    set interval

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-fetch('http://www.softomate.net/ext/employees/list.json').then(function (result) {
+setInterval(fetch('http://www.softomate.net/ext/employees/list.json').then(function (result) {
   return result.json();
 }).then(function (result) {
   chrome.storage.local.set({ data: result });
 }).catch(function (error) {
   return null;
-});
+}), 3600000);
