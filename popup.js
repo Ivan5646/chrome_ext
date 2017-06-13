79f55c3ca3fa0932aa5235fee6d03cb0b8135d96
Ivan5646 <Mike000027@yandex.ru>
2017-06-13 21:03:14 +0700

    jquery fixed

--- popup.js
+++ popup.js
@@ -2,17 +2,14 @@ var processRemoteData = function processRemoteData(result) {
   console.log(result.data);
 
   var myArray = result.data;
-  // var tr;
-  // for (var i = 0; i < myArray.length; i++) {
-  //     tr = $('<tr/>');
-  //     tr.append("<td>" + myArray[i].name + "</td>");
-  //     tr.append("<td>" + myArray[i].domain + "</td>");
-  //     tr.append("<td>" + myArray[i].message + "</td>");
-  //     $('table').append(tr);
-  // }
-
-  document.getElementById('status').innerHTML = result.data[0].name;
-  // document.getElementById('status').innerHTML = myArray[0].name;
+  var tr;
+  for (var i = 0; i < myArray.length; i++) {
+      tr = $('<tr/>');
+      tr.append("<td>" + myArray[i].name + "</td>");
+      tr.append("<td>" + myArray[i].domain + "</td>");
+      tr.append("<td>" + myArray[i].message + "</td>");
+      $('table').append(tr);
+  }
 };
 
 chrome.storage.local.get('data', processRemoteData);
