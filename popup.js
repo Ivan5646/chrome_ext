96a763a02236b6c7f631c8d7ef30beb85c5698b5
Ivan5646 <Mike000027@yandex.ru>
2017-06-14 05:33:17 +0700

    error

--- popup.js
+++ popup.js
@@ -7,8 +7,10 @@ var processRemoteData = function processRemoteData(state) {
 
   if (state.pending == true) {
     $('#status').append('<img id="preloader" src="img/preloader.gif" width="30px" height="30px" />')
-  }else {
-    // $('#preloader').remove();
+  }else if (state.error == "error") {
+    $('#status').append('<img id="error" src="img/error.png" width="30px" height="30px" />')
+  } else {
+    // $('#preloader').remove(); 
     let text = '';
     state.data.forEach((item) => text += item.name + ' ');
     $('#status').text(text);
@@ -16,7 +18,7 @@ var processRemoteData = function processRemoteData(state) {
 };
 
 chrome.storage.local.get('state', (result) => processRemoteData(result.state));
-chrome.storage.onChanged.addListener((result) => processRemoteData(result.state.newValue));
+chrome.storage.onChanged.addListener((result) => processRemoteData(result.state.newValue)); //newValue?
 
 $('#refreshButton').on('click', (event) => {
   event.stopPropagation();
