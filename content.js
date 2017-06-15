706e23d0f048a9c7f42717c19859c9c5b3bb0392
Ivan5646 <Mike000027@yandex.ru>
2017-06-16 05:05:30 +0700

    find

--- content.js
+++ content.js
@@ -2,12 +2,18 @@ console.log("hi from content.js");
 
 var processRemoteData = function processRemoteData(state) {
 
-  for (var i = 0; i < state.data.length; i++) {
-    if (state.data[i].domain == location.host) {
-      alert(state.data[i].message);
-    }
-  }
+  let found = result.state.data.find(i => location.host.indexOf(i.domain) >= 0);
+  console.log(found);
+  // if (found) {
+  //   alert(found.message);
+  // }
 
+  // for (var i = 0; i < state.data.length; i++) {
+  //   if (state.data[i].domain == location.host) {
+  //     alert(state.data[i].message);
+  //   }
+  // }
+  
 };
 
 chrome.storage.local.get('state', (result) => processRemoteData(result.state));
