912a8439407ae93eb7496dd78fe9b8500326cf0a
Ivan5646 <Mike000027@yandex.ru>
2017-06-15 23:39:30 +0700

    content.js not working2

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ console.log("hi from content.js");
 var processRemoteData = function processRemoteData(state) {
 
   for (var i = 0; i < state.data.length; i++) {
-    if (state.data[i].domain == (location.host) {
+    if (state.data[i].domain == location.host) {
       alert(state.data.[i]message);
     }
   }
