126a75cd5826a2f390c814022015d0d1fea44c23
Ivan5646 <Mike000027@yandex.ru>
2017-06-16 05:09:14 +0700

    alert ok

--- content.js
+++ content.js
@@ -2,18 +2,11 @@ console.log("hi from content.js");
 
 var processRemoteData = function processRemoteData(state) {
 
-  let found = result.state.data.find(i => location.host.indexOf(i.domain) >= 0);
+  let found = state.data.find(i => location.host.indexOf(i.domain) >= 0);
   console.log(found);
-  // if (found) {
-  //   alert(found.message);
-  // }
-
-  // for (var i = 0; i < state.data.length; i++) {
-  //   if (state.data[i].domain == location.host) {
-  //     alert(state.data[i].message);
-  //   }
-  // }
-  
+  if (found) {
+    alert(found.message);
+  }  
 };
 
 chrome.storage.local.get('state', (result) => processRemoteData(result.state));
